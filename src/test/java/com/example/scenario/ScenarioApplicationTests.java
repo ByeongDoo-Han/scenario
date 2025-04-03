9cf9293f0aa3a2a22d54e50921b8d773f75a4174
byeongdoohan <quden04@gmail.com>
2025-04-03 17:04:33 +0900

    init project

---
+++ src/test/java/com/example/scenario/ScenarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scenario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
