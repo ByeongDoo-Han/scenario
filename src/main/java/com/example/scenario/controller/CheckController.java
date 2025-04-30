fc6d53af8574f47e6e3d5e245c446804b5643271
byeongdoohan <quden04@gmail.com>
2025-04-30 22:37:31 +0900

    check 수정

--- src/main/java/com/example/scenario/controller/CheckController.java
+++ src/main/java/com/example/scenario/controller/CheckController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class CheckController {
+    
     @GetMapping("/check")
     public String checkServer(){
         return "check";
