4248344e67298ca4b83005bbc2c41eb287f3d950
byeongdoohan <quden04@gmail.com>
2025-06-13 21:57:40 +0900

    log 내용 변경

--- src/main/java/com/example/scenario/service/CourseService.java
+++ src/main/java/com/example/scenario/service/CourseService.java
@@ -46,30 +46,30 @@ public class CourseService {
 
     public synchronized SubmitLectureResult buyCourse(final Long id, final Long userId) {
         Course course = courseRepository.findById(id).orElseThrow(
-            () -> new NoSuchElementException("존재하지 않는 쿠폰입니다.")
+            () -> new NoSuchElementException("존재하지 않는 강의입니다..")
         );
         try{
             course.decrease(1L);
             courseRepository.save(course);
-            log.info("userId: {} ✅ 쿠폰 발급 성공", userId);
+            log.info("userId: {} ✅ 수강신청 성공", userId);
             return new SubmitLectureResult(userId, "success");
         } catch (RuntimeException e){
-            log.info("userId: {} ❌ 쿠폰 재고 없음", userId);
+            log.info("userId: {} ❌ 수강신청 실패", userId);
             return new SubmitLectureResult(userId,"fail");
         }
     }
 
     public SubmitLectureResult notSyncBuyCourse(final Long id, final Long userId) {
         Course course = courseRepository.findById(id).orElseThrow(
-            () -> new NoSuchElementException("존재하지 않는 쿠폰입니다.")
+            () -> new NoSuchElementException("존재하지 않는 강의입니다.")
         );
         try{
             course.decrease(1L);
             courseRepository.save(course);
-            log.info("userId: {} ✅ 쿠폰 발급 성공", userId);
+            log.info("userId: {} ✅ 수강신청 성공.", userId);
             return new SubmitLectureResult(userId, "success");
         } catch (RuntimeException e){
-            log.info("userId: {} ❌ 쿠폰 재고 없음", userId);
+            log.info("userId: {} ❌ 수강신청 실패.", userId);
             return new SubmitLectureResult(userId,"fail");
         }
     }
