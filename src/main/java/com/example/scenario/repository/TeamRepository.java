f25393b2fe83dfdc48c60b2fee94dcae393b19a2
byeongdoohan <quden04@gmail.com>
2025-06-28 23:24:49 +0900

    추가

--- src/main/java/com/example/scenario/repository/TeamRepository.java
+++ src/main/java/com/example/scenario/repository/TeamRepository.java
@@ -1,4 +1,9 @@
 package com.example.scenario.repository;
 
-public interface TeamRepository {
+import com.example.scenario.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
