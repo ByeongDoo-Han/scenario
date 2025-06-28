6e1289ad194fcc271bfbe19d1844d55aa64ba91d
byeongdoohan <quden04@gmail.com>
2025-06-28 23:43:13 +0900

    test

--- src/main/java/com/example/scenario/service/NplusOneService.java
+++ src/main/java/com/example/scenario/service/NplusOneService.java
@@ -1,14 +1,18 @@
 package com.example.scenario.service;
 
+import com.example.scenario.entity.Team;
 import com.example.scenario.repository.MemberRepository;
 import com.example.scenario.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NplusOneService {
 
     private final TeamRepository teamRepository;
+    
     private final MemberRepository memberRepository;
 }
