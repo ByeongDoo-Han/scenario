79e65e41412424567000f02bdff99404a058ddf4
byeongdoohan <quden04@gmail.com>
2025-06-28 23:20:05 +0900

    메서드 추가

--- src/main/java/com/example/scenario/controller/NplusOneController.java
+++ src/main/java/com/example/scenario/controller/NplusOneController.java
@@ -1,11 +1,21 @@
 package com.example.scenario.controller;
 
+import com.example.scenario.entity.Member;
+import com.example.scenario.service.NplusOneService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class NplusOneController {
 
+    private final NplusOneService nplusOneService;
+    public List<Member> getAllMember(){
+        return nplusOneService.getAllMember();
+    }
 }
